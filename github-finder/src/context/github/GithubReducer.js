801caf6cad6bfc598dd66f78c96a6b7ff81b862b
Kunal-Sethi01 <sethikunal0108@gmail.com>
2022-07-29 17:40:36 +0530

    Added Reducers to the code.

---
+++ github-finder/src/context/github/GithubReducer.js
@@ -0,0 +1,23 @@
+const githubReducer = (state, action) => {
+  switch (action.type) {
+    case "GET_USERS":
+      return {
+        ...state,
+        users: action.payload,
+        loading: false,
+      };
+    case "SET_LOADING":
+      return {
+        ...state,
+        loading: true,
+      };
+    case "CLEAR_SEARCH":
+      return {
+        ...state,
+        users: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+export default githubReducer;
